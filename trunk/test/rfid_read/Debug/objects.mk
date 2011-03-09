870e03fcebc59cd50a147d1cca38291ba8f4d267
zlrenner@gmail.com <zlrenner@gmail.com@6312de72-f2c9-d921-6426-d5bbbed1faae>
2011-03-09 22:48:52 +0000

    Test case for RFID driver read functionality

---
+++ trunk/test/rfid_read/Debug/objects.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL := cmd.exe
+
+USER_OBJS :=
+
+LIBS :=
+
+BUILDCMD := arm-none-eabi-gcc\ -mthumb\ -mcpu=cortex-m3\ -specs=bare.specs

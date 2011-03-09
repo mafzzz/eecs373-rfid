51fb06cae3e29e04511792f0d8f6ad626787cd00
zlrenner@gmail.com <zlrenner@gmail.com@6312de72-f2c9-d921-6426-d5bbbed1faae>
2011-03-09 22:48:52 +0000

    Test case for RFID driver read functionality

---
+++ test/rfid_read/Debug/sources.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+S_SRCS := 
+OBJ_SRCS := 
+MEMORYMAP := 
+OBJS := 
+C_DEPS := 
+SRECFILES := 
+IHEXFILES := 
+BINFILES := 
+LISTINGS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+CMSIS \
+CMSIS/startup_gcc \
+
---
+++ test/rfid_read/main.c
@@ -0,0 +1,8 @@
+/* Test case for RFID driver read functionality */
+
+int main() 
+{
+	/* TODO: RFID read test */
+	while(1);
+	return 0;
+}

9ee5da5eef277725bbc794b46f9e26895de5f410
zlrenner@gmail.com <zlrenner@gmail.com@6312de72-f2c9-d921-6426-d5bbbed1faae>
2011-03-18 06:16:17 +0000

    Added display driver and test.

---
+++ src/include/device/disp.h
@@ -0,0 +1,42 @@
+#ifndef __DISP_H__
+#define __DISP_H__
+#include "driver/core_uart_apb.h"
+#include "devices.h"
+
+
+#define DISP_UART_BASE	0x40050000UL
+#define DISP_BAUD_RATE	115200
+#define DISP_BAUD_VALUE ((FAB_CLK / (DISP_BAUD_RATE*16))-1)
+
+/* Display commands */
+#define DISP_CMD_START		0x7C
+#define DISP_CMD_CLEAR		0x00
+#define DISP_CMD_MOVEX		0x18
+#define DISP_CMD_MOVEY		0x19
+#define DISP_CMD_SETPIXEL	0x10
+#define DISP_CMD_INVERT		0x12
+#define DISP_CMD_SETPIXEL	0x10
+#define DISP_CMD_DEMO		0x04
+#define DISP_CMD_BACKLIGHT	0x02
+
+#define DISP_MAX_STRLEN 100 /* For disp_print */
+
+UART_instance_t g_disp_uart;
+
+typedef struct {
+	uint8_t *data;
+	uint32_t size;
+} disp_req_t;
+
+int disp_init();
+void disp_clear();
+void disp_draw_pixel(uint8_t x, uint8_t y);
+void disp_set_pixel(uint8_t x, uint8_t y, uint8_t val);
+void disp_putc(const char c);
+int disp_printf(const char * fmt, ...);
+void disp_newline();
+void disp_invert();
+void disp_demo();
+void disp_set_backlight(int level);
+
+#endif
---
+++ src/include/devices.h
@@ -0,0 +1,6 @@
+#ifndef __DEVICES_H__
+#define __DEVICES_H__
+
+#define FAB_CLK 100000000
+
+#endif

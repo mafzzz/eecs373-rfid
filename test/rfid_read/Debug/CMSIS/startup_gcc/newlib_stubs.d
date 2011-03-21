d7d67b84558c7d0a19741103879b671eef083a56
zlrenner@gmail.com <zlrenner@gmail.com@6312de72-f2c9-d921-6426-d5bbbed1faae>
2011-03-21 18:31:14 +0000

    Actually added list, updates to disp_test project

--- test/rfid_read/Debug/CMSIS/startup_gcc/newlib_stubs.d
+++ test/rfid_read/Debug/CMSIS/startup_gcc/newlib_stubs.d
@@ -1,2 +1,2 @@
-CMSIS/startup_gcc/newlib_stubs.d CMSIS/startup_gcc/newlib_stubs.o:  \
+CMSIS/startup_gcc/newlib_stubs.d CMSIS/startup_gcc/newlib_stubs.o: \
  ../CMSIS/startup_gcc/newlib_stubs.c
--- test/rfid_read/Debug/objects.mk
+++ test/rfid_read/Debug/objects.mk
@@ -2,10 +2,6 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL := cmd.exe
-
 USER_OBJS :=
 
 LIBS :=
-
-BUILDCMD := arm-none-eabi-gcc\ -mthumb\ -mcpu=cortex-m3\ -specs=bare.specs

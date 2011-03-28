79a4b9141db27b2a1da89721b5e395ee3f7fd690
djonik08@gmail.com <djonik08@gmail.com@6312de72-f2c9-d921-6426-d5bbbed1faae>
2011-03-28 04:52:00 +0000

    added test project for ethernet driver and added several files to src library

---
+++ trunk/src/device/network_driver.c
@@ -0,0 +1,17 @@
+/*
+ * This file provides the implementation for the network driver functions.
+ */
+#include <stdio.h>
+
+void init_network()
+{
+
+
+}
+
+
+// switch from void to some type of error_t
+void send_request();
+
+// switch to certain data_t that will be received from server
+void receive_request();
